6f5173ff044827ceb82a6c1c66a37a63e4eadae8
nanggo <yamsiri@gmail.com>
2019-10-28 00:37:30 +0900

    Make test for R/U/D

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
-
 import com.example.study.model.entity.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -16,7 +18,7 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
     @Test
     public void create() {
-       User user = new User();
+        User user = new User();
 
         user.setAccount("TestUser02");
         user.setEmail("TestUser02@email.com");
@@ -28,15 +30,47 @@ public class UserRepositoryTest extends StudyApplicationTests {
         System.out.println("newUser: " + newUser);
     }
 
+    @Test
     public void read() {
+        Optional<User> user = userRepository.findById(3L);
+
+        user.ifPresent(selectedUser -> {
+            System.out.println("User : " + selectedUser);
+            System.out.print("email : " + selectedUser.getEmail());
+        });
 
     }
 
+    @Test
     public void update() {
+        Optional<User> user = userRepository.findById(3L);
 
+        user.ifPresent(selectedUser ->{
+            selectedUser.setAccount("pppp");
+            selectedUser.setUpdatedAt(LocalDateTime.now());
+            selectedUser.setUpdatedBy("Update method()");
+            userRepository.save(selectedUser);
+        });
     }
 
+    @Test
+    @Transactional
     public void delete() {
+        Optional<User> user = userRepository.findById(1L);
 
+        Assert.assertTrue(user.isPresent());
+
+        user.ifPresent(selectedUser ->{
+            userRepository.delete(selectedUser);
+        });
+
+        Optional<User> deletedUser = userRepository.findById(1L);
+
+        Assert.assertFalse(deletedUser.isPresent());
+//        if (deletedUser.isPresent()){
+//            System.out.println("User not deleted : " + deletedUser.get());
+//        } else {
+//            System.out.println("User deleted.");
+//        }
     }
 }
